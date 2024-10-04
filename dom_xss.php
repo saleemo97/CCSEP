a2fad9e3a75410d1e67eff94f0c3ee9ee09d552c
Osama <samssal@outlook.com>
2024-10-04 20:49:10 +0800

    Updated xss DOM

--- dom_xss.php
+++ dom_xss.php
@@ -2,13 +2,15 @@
 
 <h1 class="mb-4">DOM XSS Example</h1>
 <div class="mb-3">
-    <label for="userInput" class="form-label">Enter a Message:</label>
-    <input id="userInput" type="text" class="form-control" onkeyup="showMessage()">
+    <label for="userInput" class="form-label">Enter a Message (try: <strong>&lt;img src=x onerror="alert('XSS!')"&gt;</strong>):</label>
+    <input id="userInput" type="text" class="form-control">
 </div>
-<div class="card">
+<button onclick="showMessage()" class="btn btn-primary">Submit</button>
+
+<div class="card mt-3">
     <div class="card-body">
         <h5 class="card-title">Output:</h5>
-        <p id="output" class="card-text"></p>
+        <div id="output" class="card-text"></div> <!-- This is where the user input is displayed -->
     </div>
 </div>
 
